1bee5b1e8239fd55519c8a2f86d110ee489f6e6a
Matt Ingenthron <ingenthr@cep.net>
2012-08-10 11:03:29 -0700

    Changed downloads all line: legacy, removed couchdb.

--- index.php
+++ index.php
@@ -448,11 +448,10 @@ if ($mimetype === 'application/json') {
 </div>
 {{^multiple_products}}
 <p class="cb-all-downloads">
-  Looking for <a href="/downloads-all#couchbase-server-2-0">2.0 Dev Preview</a>,
-  <a href="/downloads-all#moxi-server-1-8">Moxi Server</a>,
-  <a href="/downloads-all#couchbase-server-1-7">Membase</a>, or
-  <a href="/couchdb">CouchDB</a>?
-  <b><a href="/downloads-all" style="border-left:1px solid #EDEDE5;padding-left:20px;margin-left:20px">View all of our Downloads</a></b>
+  Looking for <a href="/downloads-all#couchbase-server-2-0">Couchbase Server 2.0 recent builds</a>,
+  <a href="/downloads-all#couchbase-server-1-7">legacy Membase</a> or other products?
+  <b><a href="/downloads-all"
+    style="border-left:2px solid #EDEDE5;padding-left:20px;margin-left:20px">View all of our downloads</a></b>.
 </p>
 <div class="container-6-inner">
   <div class="grid-4 first">
